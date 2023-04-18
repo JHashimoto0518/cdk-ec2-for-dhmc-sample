ade9e39d83a95e3cda105b38a80f895fda7f8529
Junichi Hashimoto <46337314+JHashimoto0518@users.noreply.github.com>
2023-04-19 06:49:15 +0900

    recreated cdk project

--- src/test/cdk-ec2-for-dhmc-sample.test.ts
+++ src/test/cdk-ec2-for-dhmc-sample.test.ts
@@ -1,13 +1,13 @@
 // import * as cdk from 'aws-cdk-lib';
 // import { Template } from 'aws-cdk-lib/assertions';
-// import * as Src from '../lib/src-stack';
+// import * as CdkEc2ForDhmcSample from '../lib/cdk-ec2-for-dhmc-sample-stack';
 
 // example test. To run these tests, uncomment this file along with the
-// example resource in lib/src-stack.ts
+// example resource in lib/cdk-ec2-for-dhmc-sample-stack.ts
 test('SQS Queue Created', () => {
 //   const app = new cdk.App();
 //     // WHEN
-//   const stack = new Src.SrcStack(app, 'MyTestStack');
+//   const stack = new CdkEc2ForDhmcSample.CdkEc2ForDhmcSampleStack(app, 'MyTestStack');
 //     // THEN
 //   const template = Template.fromStack(stack);
 
